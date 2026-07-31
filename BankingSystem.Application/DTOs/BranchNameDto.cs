947fd43028f9268059ba681866f0c9881c301001
fariborzvrm <fariborz.vrm@gmail.com>
2026-07-31 20:58:23 +0330

    refactor: add new dto to use fluentvalidation for branchname input

---
+++ BankingSystem.Application/DTOs/BranchNameDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem.Application.DTOs
+{
+    public class BranchNameDto
+    {
+        public string BranchName { get; set; } = string.Empty;
+    }
+}
