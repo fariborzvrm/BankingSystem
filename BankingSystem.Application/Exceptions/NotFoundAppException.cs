44528c851a2cd93e6764a1b3b1b0638bb8a96331
fariborzvrm <fariborz.vrm@gmail.com>
2026-07-31 20:58:43 +0330

    refactor: add new custom exception

---
+++ BankingSystem.Application/Exceptions/NotFoundAppException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem.Application.Exceptions
+{
+    public class NotFoundAppException : Exception
+    {
+        public NotFoundAppException(string message) : base(message)  { }
+    }
+}
