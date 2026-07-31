8c39352bf06c03e8520b75fcf3f397d7b3f139a4
fariborzvrm <fariborz.vrm@gmail.com>
2026-07-31 20:55:50 +0330

    feat: add BankAccountValidator to use FluentValidation

---
+++ BankingSystem/Validators/BankAccountValidator.cs
@@ -0,0 +1,17 @@
+﻿using BankingSystem.Application.DTOs;
+using FluentValidation;
+
+namespace BankingSystem.API.Validators
+{
+    public class BankAccountValidator : AbstractValidator<BranchNameDto>
+    {
+        public BankAccountValidator() {
+
+            RuleFor(x => x.BranchName)
+                .NotEmpty().WithMessage("Branch name is required.")
+                .MaximumLength(20).WithMessage("Branch name must not exceed 20 characters.")
+                .MinimumLength(3).WithMessage("Branch name must be at least 3 characters long.");               
+
+        }
+    }
+}
